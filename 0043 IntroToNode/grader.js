d918a28eccb2998f19173ab6a67a1cf961ecdbcc
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-09 10:42:22 -0400

    added code to test the function and have the file do something visible in node

--- 0043 IntroToNode/grader.js
+++ 0043 IntroToNode/grader.js
@@ -6,3 +6,9 @@ function average(scores) {
   result = Math.round(result / scores.length);
   console.log(result);
 }
+
+var scores = [90, 98, 89, 100, 100, 86, 94];
+average(scores); //should return 94
+
+var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
+average(scores2); //should return 68
