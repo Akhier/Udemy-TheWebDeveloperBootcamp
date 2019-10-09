eee10515222026c72773bf34be13c9776c0cf0ea
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-09 11:30:15 -0400

    The comment is how it is done in the exercise answer. I just found the .fake function and used it instead of trying to string together the various faker functions.

--- 0043 IntroToNode/MyShop/listProducts.js
+++ 0043 IntroToNode/MyShop/listProducts.js
@@ -7,3 +7,5 @@ console.log("====================");
 for (let i = 0; i < 10; i++) {
   console.log(faker.fake("{{commerce.productName}} - ${{commerce.price}}"));
 }
+
+//console.log(faker.commerce.productName() + " - $" + faker.commerce.price());
