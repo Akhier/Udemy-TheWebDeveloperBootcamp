7e292291e54d198e33fab6f46fefaef821cc41fd
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-09 10:46:42 -0400

    Watched the answer given and this was how he did it. Looking back at the excercise requirements it did not mention logging the results.

--- 0043 IntroToNode/grader.js
+++ 0043 IntroToNode/grader.js
@@ -4,11 +4,12 @@ function average(scores) {
     result += score;
   });
   result = Math.round(result / scores.length);
-  console.log(result);
+  return result;
 }
 
+console.log("Average Score");
 var scores = [90, 98, 89, 100, 100, 86, 94];
-average(scores); //should return 94
+console.log(average(scores)); //should return 94
 
 var scores2 = [40, 65, 77, 82, 80, 54, 73, 63, 95, 49];
-average(scores2); //should return 68
+console.log(average(scores2)); //should return 68
