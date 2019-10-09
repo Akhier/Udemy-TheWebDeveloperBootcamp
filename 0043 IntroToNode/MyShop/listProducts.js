14fb5f4d3d6aacc34a3363ddb99d37e247dd0745
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-09 11:23:23 -0400

    It was almost correct. commerce.price does not include the dollar sign and thinking about it I agree with this. After all, the US dollar might be a major player in the finance world but there are other currencies with their own symbols.

--- 0043 IntroToNode/MyShop/listProducts.js
+++ 0043 IntroToNode/MyShop/listProducts.js
@@ -5,5 +5,5 @@ console.log("WELCOME TO MY SHOP!");
 console.log("====================");
 
 for (let i = 0; i < 10; i++) {
-  console.log(faker.fake("{{commerce.productName}} - {{commerce.price}}"));
+  console.log(faker.fake("{{commerce.productName}} - ${{commerce.price}}"));
 }
