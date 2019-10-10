11782be1324ef295b1bb0b99de588135823d2e4a
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-10 10:50:22 -0400

    Because I am using a local server this strays from the video. Cloud9 has shutdown so the ability to use the original technology involved was not present and instead of using another cloud service I went with the do it myself option. This code was taken from the following page https://zarkom.net/blogs/windows-local-coding-environment-1204

--- 0044 IntroToExpress/app.js
+++ 0044 IntroToExpress/app.js
@@ -4,3 +4,8 @@ var app = express();
 app.get("/", function(req, res){
   res.send("Hi there!");
 });
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
