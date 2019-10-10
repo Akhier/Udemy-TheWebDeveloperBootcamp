f1440bb3a7a1e0f554a535028ca6376ac0f0caef
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-10 10:56:04 -0400

    Added two more routes. One to /bye and the other to /dog

--- 0044 IntroToExpress/app.js
+++ 0044 IntroToExpress/app.js
@@ -5,6 +5,14 @@ app.get("/", function(req, res){
   res.send("Hi there!");
 });
 
+app.get("/bye", function(req, res){
+  res.send("Goodbye!");
+});
+
+app.get("/dog", function(req, res){
+  res.send("MEOW!");
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
