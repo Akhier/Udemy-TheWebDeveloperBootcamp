1e2d186c5987521243169eab47dba1b2021b0d5b
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-10 11:35:42 -0400

    added root route

--- 0045 Express Basics Exercise/app.js
+++ 0045 Express Basics Exercise/app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(req, res){
+  app.send("Hi there, welcome to my assignment!");
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
