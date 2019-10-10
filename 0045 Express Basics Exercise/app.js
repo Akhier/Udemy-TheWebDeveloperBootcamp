1c9da366eb36f11df64bd7369c40a0b7c1145e60
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-10 11:42:45 -0400

    notice it wasn't saved

--- 0045 Express Basics Exercise/app.js
+++ 0045 Express Basics Exercise/app.js
@@ -5,6 +5,21 @@ app.get("/", function(req, res){
   app.send("Hi there, welcome to my assignment!");
 });
 
+app.get("/speak/:animal", function(req, res){
+  var animal = req.params.animal;
+  var line = "the " + animal + " says '";
+  if (animal === "pig") {
+    line += "Oink'";
+  } else if (animal === "cow") {
+    line += "Moo'";
+  } else if (animal === "dog") {
+    line += "Woof Woof!'";
+  } else {
+    line = "Sorry, page not found...What are you doing with your life?";
+  }
+  app.send(line);
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
