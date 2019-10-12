92399c7a553781e2dfb741a1f041f2b36d1a9039
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-12 11:06:07 -0400

    Add home route

--- 0046 EJSDemo/app.js
+++ 0046 EJSDemo/app.js
@@ -1,6 +1,10 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function(req, res){
+  res.send("Welcome to the home page!");
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
