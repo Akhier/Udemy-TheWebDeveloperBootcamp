ca5297d549bd2c17da5315d7c0e3475037d28730
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-12 11:06:46 -0400

    example of html within route

--- 0046 EJSDemo/app.js
+++ 0046 EJSDemo/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-  res.send("Welcome to the home page!");
+  res.send("<h1>Welcome to the home page!</h1><h2>blah blah</h2>");
 });
 
 var port = process.env.PORT || 3000;
