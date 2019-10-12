c2b50a2bbbd178c58e4d2eaa93113537825df796
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-12 11:13:06 -0400

    Add home.ejs and res.render to show how to make more complex webpages than allowed with just res.send, did not install ejs yet

--- 0046 EJSDemo/app.js
+++ 0046 EJSDemo/app.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function(req, res){
-  res.send("<h1>Welcome to the home page!</h1><h2>blah blah</h2>");
+  res.render("home.ejs");
 });
 
 var port = process.env.PORT || 3000;
