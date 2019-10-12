e8427a32576a433ce3aaa35ff4155fb45249cd11
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-12 13:12:12 -0400

    Add simple home route

--- 0047 PostRequestDemo/app.js
+++ 0047 PostRequestDemo/app.js
@@ -3,6 +3,10 @@ var app = express();
 
 app.set("view engine", "ejs");
 
+app.get("/", function(req, res){
+  res.render("home");
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("Server Has Started!");
