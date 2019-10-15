f539f822147efe2fe0331886c0aa6f1dc32a3804
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-15 18:36:08 -0400

    add compgrounds route

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
   res.render("landing");
 });
 
+app.get("/campgrounds", function(req, res){
+
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("The YelpCamp Server Has Started!");
