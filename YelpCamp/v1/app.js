19266d1bc4621a7a35d383e31c950405da906270
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-15 19:11:37 -0400

    push in new campground and redirect

--- YelpCamp/v1/app.js
+++ YelpCamp/v1/app.js
@@ -22,6 +22,9 @@ app.get("/campgrounds", function(req, res){
 app.post("/campgrounds", function(req, res){
   var name = req.body.name;
   var image = req.body.image;
+  var newCampground = {name: name, image: image};
+  campgrounds.push(newCampground);
+  res.redirect("/campgrounds");
 });
 
 app.get("/campgrounds/new", function(req, res){
