dee4d361fb588af8e8c66b146e0e1ca087a358ea
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-16 10:52:14 -0400

    make a cat schema for mongoose

--- 0048 Introduction to Mongoose/cats.js
+++ 0048 Introduction to Mongoose/cats.js
@@ -1,3 +1,9 @@
 var mongoose = require("mongoose");
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/cat_app", { useNewUrlParser: true });
+
+var catSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  temperament: String
+});
