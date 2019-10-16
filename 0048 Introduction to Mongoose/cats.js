1b78f4419370dcaf0db181a74aa337bcfdb5fa42
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-16 11:03:19 -0400

    finding all the cats

--- 0048 Introduction to Mongoose/cats.js
+++ 0048 Introduction to Mongoose/cats.js
@@ -10,17 +10,27 @@ var catSchema = new mongoose.Schema({
 
 var Cat = mongoose.model("Cat", catSchema);
 
-var george = new Cat({
-  name: "Mrs. Norris",
-  age: 7,
-  temperament: "Evil"
-});
+// var george = new Cat({
+//   name: "Mrs. Norris",
+//   age: 7,
+//   temperament: "Evil"
+// });
 
-george.save(function(err, cat){
+// george.save(function(err, cat){
+//   if(err){
+//     console.log("Something went Wrong.");
+//   } else {
+//     console.log("We just saved a cat to the DB:");
+//     console.log(cat);
+//   }
+// });
+
+Cat.find({}, function(err, cats){
   if(err){
-    console.log("Something went Wrong.");
+    console.log("Oh no, Error.");
+    console.log(err);
   } else {
-    console.log("We just saved a cat to the DB:");
-    console.log(cat);
+    console.log("All the Cats.");
+    console.log(cats);
   }
 });
