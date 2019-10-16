2f85b45cdb2859b88046d1cc0496b954453b7470
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-16 10:57:35 -0400

    create a cat and save it to the database

--- 0048 Introduction to Mongoose/cats.js
+++ 0048 Introduction to Mongoose/cats.js
@@ -9,3 +9,18 @@ var catSchema = new mongoose.Schema({
 });
 
 var Cat = mongoose.model("Cat", catSchema);
+
+var george = new Cat({
+  name: "George",
+  age: 11,
+  temperament: "Grouchy"
+});
+
+george.save(function(err, cat){
+  if(err){
+    console.log("Something went Wrong.");
+  } else {
+    console.log("We just saved a cat to the DB:");
+    console.log(cat);
+  }
+});
