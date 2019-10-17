a058eaec197e842fe702188a79796383ad4bda5e
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-17 12:17:13 -0400

    whoops, capatilized a C there.

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -30,9 +30,10 @@ var Campground = mongoose.model("Campground", campgroundSchema);
 
 // var campgrounds = [
 //   {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-//   {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+//   {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg", description: "This is a huge granite hill, no bathrooms. No water. Beautiful granite!"},
 //   {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-//   {name: "Cloud's Rest", image: "https://farm4.staticflickr.com/3088/2731213504_9effdeecc8.jpg"}
+//   {name: "Cloud's Rest", image: "https://farm4.staticflickr.com/3088/2731213504_9effdeecc8.jpg"},
+//   {name: "Campground with trees", image: "https://farm3.staticflickr.com/2288/5808881553_8c3214a9ce.jpg", description: "This place has lots of trees and also lots of other stuff."}
 // ]
 
 app.get("/", function(req, res){
@@ -74,7 +75,7 @@ app.get("/campgrounds/:id", function(req, res){
     } else {
       res.render("show", {campground: foundCampground});
     }
-  }),
+  })
 });
 
 var port = process.env.PORT || 3000;
