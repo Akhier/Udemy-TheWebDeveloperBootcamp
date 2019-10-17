580d427b6cb2a23f5cbf8ba1e2e55855868c6eb7
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-17 11:31:25 -0400

    added a couple campgrounds and changing the route /campgrounds to pull from the db

--- YelpCamp/v2/app.js
+++ YelpCamp/v2/app.js
@@ -14,6 +14,17 @@ var campgroundSchema = new mongoose.Schema({
 });
 var Campground = mongoose.model("Campground", campgroundSchema);
 
+// Campground.create({
+//   name: "Granite Hill",
+//   image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"
+// }, function(err, campground){
+//   if(err){
+//     console.log(err);
+//   } else {
+//     console.log(campground);
+//   }
+// });
+
 var campgrounds = [
   {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
   {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
@@ -25,7 +36,13 @@ app.get("/", function(req, res){
 });
 
 app.get("/campgrounds", function(req, res){
-  res.render("campgrounds", {campgrounds:campgrounds});
+  Campground.find({}, function(err, campgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds", {campgrounds:campgrounds});
+    }
+  })
 });
 
 app.post("/campgrounds", function(req, res){
