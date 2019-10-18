990d1774f435cdb55dedb33724c071468bc02e18
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-18 12:10:43 -0400

    more setup

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -2,3 +2,9 @@ var express     = require("express"),
     app         = express(),
     bodyParser  = require("body-parser"),
     mongoose    = require("mongoose");
+
+mongoose.set('useUnifiedTopology', true);
+mongoose.connect("mongodb://localhost/restful_blog_app", , { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
