b0a11fff02b2b0f05be27b32bf20b278c1a4ea04
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-18 12:11:39 -0400

    add the app.listen

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -8,3 +8,8 @@ mongoose.connect("mongodb://localhost/restful_blog_app", , { useNewUrlParser: tr
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log("Server Has Started!");
+});
