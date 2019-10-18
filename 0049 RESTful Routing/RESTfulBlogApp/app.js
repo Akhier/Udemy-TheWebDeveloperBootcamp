740e8a8366c528ddd7fb4b77b1a733108802e040
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-18 12:15:57 -0400

    create and compile blog schema. Learned how to put a default value. Instead of just created: Date you can put created: {type: Date, default: Date.now} and it will have the date type while defaulting to the current date

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -9,7 +9,13 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-
+var blogSchema = new mongoose.Schema({
+  title: String,
+  image: String,
+  body: String,
+  created: {type: Date, default: Date.now}
+});
+var Blog = mongoose.model("Blog", blogSchema);
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
