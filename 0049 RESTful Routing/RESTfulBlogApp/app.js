49e1cbf1503c9d0c895c594ccfe6af66918a9f88
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-18 12:12:30 -0400

    extra comma removed

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -4,10 +4,12 @@ var express     = require("express"),
     mongoose    = require("mongoose");
 
 mongoose.set('useUnifiedTopology', true);
-mongoose.connect("mongodb://localhost/restful_blog_app", , { useNewUrlParser: true });
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+
+
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
