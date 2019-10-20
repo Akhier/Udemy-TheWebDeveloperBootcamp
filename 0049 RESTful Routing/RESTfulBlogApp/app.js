47bc6b571c7c9f96c169b672cad24e9b431cf189
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-20 13:30:09 -0400

    add the require and app.use for method-override

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -1,13 +1,15 @@
 var express     = require("express"),
-    app         = express(),
-    bodyParser  = require("body-parser"),
-    mongoose    = require("mongoose");
+app             = express(),
+bodyParser      = require("body-parser"),
+mongoose        = require("mongoose"),
+methodOverride  = require("method-override");
 
 mongoose.set('useUnifiedTopology', true);
 mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 var blogSchema = new mongoose.Schema({
   title: String,
