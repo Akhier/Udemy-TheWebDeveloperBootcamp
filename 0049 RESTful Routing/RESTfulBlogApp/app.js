116f68b53fecccffe48b508dbe372abecd07accd
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-20 13:36:01 -0400

    finish the update routhe with findByIdAndUpdate

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -68,7 +68,13 @@ app.get("/blogs/:id/edit", function(req, res){
 });
 
 app.put("/blogs/:id", function(req, res){
-  
+  Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+    if(err){
+      res.redirect("/blogs");
+    } else {
+      res.redirect("/blogs/" + req.params.id);
+    }
+  });
 });
 
 var port = process.env.PORT || 3000;
