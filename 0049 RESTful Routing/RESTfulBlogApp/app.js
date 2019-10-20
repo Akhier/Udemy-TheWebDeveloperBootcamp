c168a7f3d959d48b854b1c1c306c8a88896b97ad
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-20 12:06:11 -0400

    make an error just render new again. If no error redirect to /blogs

--- 0049 RESTful Routing/RESTfulBlogApp/app.js
+++ 0049 RESTful Routing/RESTfulBlogApp/app.js
@@ -37,7 +37,11 @@ app.get("/blogs/new", function(req, res){
 
 app.post("/blogs", function(req, res){
   Blog.create(req.body.blog, function(err, newBlog){
-
+    if(err){
+      res.render("new")
+    } else {
+      res.redirect("/blogs");
+    }
   });
 });
 
