33439494089a6f0f597f77fd4201e8742217c7e6
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-29 14:57:26 -0400

    making the post route for register

--- 0050 Authentication/AuthDemo/app.js
+++ 0050 Authentication/AuthDemo/app.js
@@ -33,6 +33,10 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.post("/register", function(req, res){
+  res.send("register post route");
+});
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("server started");
 });
