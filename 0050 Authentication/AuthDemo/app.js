e94a991c758a98272db5b2f9c222ae007436f115
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-29 13:50:53 -0400

    create secret route and ejs file

--- 0050 Authentication/AuthDemo/app.js
+++ 0050 Authentication/AuthDemo/app.js
@@ -7,6 +7,10 @@ app.get("/", function(req, res){
   res.render("home");
 });
 
+app.get("/secret", function(req, res){
+  res.render("secret");
+});
+
 app.listen(process.env.PORT || 3000, function(){
   console.log("server started");
 });
