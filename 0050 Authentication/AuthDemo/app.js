e074f812d36afecc9ac9e17feb4a6a7d3e361b1b
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-29 13:50:00 -0400

    app.listen code

--- 0050 Authentication/AuthDemo/app.js
+++ 0050 Authentication/AuthDemo/app.js
@@ -6,3 +6,7 @@ app.set("view engine", "ejs");
 app.get("/", function(req, res){
   res.render("home");
 });
+
+app.listen(process.env.PORT || 3000, function(){
+  console.log("server started");
+});
