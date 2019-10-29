4d94cf0d281d05a70a1b52dcdaadbedbc1b74b23
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-29 15:03:04 -0400

    code for authenticating the user

--- 0050 Authentication/AuthDemo/app.js
+++ 0050 Authentication/AuthDemo/app.js
@@ -36,7 +36,13 @@ app.get("/register", function(req, res){
 
 app.post("/register", function(req, res){
   User.register(new User({username: req.body.username}), req.body.password, function(err, user){
-
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.render("/secret");
+    });
   });
 });
 
