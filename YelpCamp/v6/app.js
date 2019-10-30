0eae782304ff96313ea515b04102a5bc13c88170
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-30 12:52:58 -0400

    register the new user

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -102,6 +102,19 @@ app.get("/register", function(req, res){
   res.render("register");
 });
 
+app.post("/register", function(req, res){
+  var newUser = new User({username: req.body.username});
+  User.register(newUser, req.body.password, function(err, user){
+    if(err){
+      console.log(err);
+      return res.render("register");
+    }
+    passport.authenticate("local")(req, res, function(){
+      res.redirect("/campgrounds");
+    })
+  })
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
   console.log("The YelpCamp Server Has Started!");
