8a2355a6edc4c934d1a1e47997cda0dc4186d995
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-30 13:04:49 -0400

    added the authentication middleware to the login post route

--- YelpCamp/v6/app.js
+++ YelpCamp/v6/app.js
@@ -119,9 +119,10 @@ app.get("/login", function(req, res){
   res.render("login");
 });
 
-app.post("/login", function(req, res){
-  
-});
+app.post("/login", passport.authenticate("local", {
+  successRedirect: "/campgrounds",
+  failureRedirect: "/login"
+}) function(req, res){});
 
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
