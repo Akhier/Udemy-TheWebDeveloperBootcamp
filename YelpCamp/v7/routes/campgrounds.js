51399b75193a4e146f334864a66394ebb4fb53e7
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-10-31 13:34:42 -0400

    Just directly copied and pasted the campground routes into routes/campgrounds.js

---
+++ YelpCamp/v7/routes/campgrounds.js
@@ -0,0 +1,37 @@
+app.get("/campgrounds", function(req, res){
+  Campground.find({}, function(err, allCampgrounds){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/index", {campgrounds:allCampgrounds});
+    }
+  })
+});
+
+app.post("/campgrounds", function(req, res){
+  var name = req.body.name;
+  var image = req.body.image;
+  var desc = req.body.description;
+  var newCampground = {name: name, image: image, description: desc};
+  Campground.create(newCampground, function(err, newlyCreated){
+    if(err){
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
+});
+
+app.get("/campgrounds/new", function(req, res){
+  res.render("campgrounds/new");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+  Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+    if(err){
+      console.log(err);
+    } else {
+      res.render("campgrounds/show", {campground: foundCampground});
+    }
+  });
+});
