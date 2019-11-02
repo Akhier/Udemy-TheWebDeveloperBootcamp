eb8e9da70334cc66dbb8f17206a605069a94feeb
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-11-02 14:29:19 -0400

    add the middleware to update and delete as well

--- YelpCamp/v10/routes/campgrounds.js
+++ YelpCamp/v10/routes/campgrounds.js
@@ -50,7 +50,7 @@ router.get("/:id/edit", checkCampgroundOwnership, function(req, res){
   });
 });
 
-router.put("/:id", function(req, res){
+router.put("/:id", checkCampgroundOwnership, function(req, res){
   Campground.findByIdAndUpdate(req.params.id, req.body.campground, function(err, updatedCampground){
     if(err){
       res.redirect("/campgrounds");
@@ -60,7 +60,7 @@ router.put("/:id", function(req, res){
   });
 });
 
-router.delete("/:id", function(req, res){
+router.delete("/:id", checkCampgroundOwnership, function(req, res){
   Campground.findByIdAndRemove(req.params.id, function(err){
     if(err){
       res.redirect("/campgrounds");
