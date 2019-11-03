901d5056c1199227b29f8a24a0448c89866c4618
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-11-03 12:16:55 -0500

    write the delete route

--- YelpCamp/v10/routes/comments.js
+++ YelpCamp/v10/routes/comments.js
@@ -56,7 +56,13 @@ router.put("/:comment_id", function(req, res){
 });
 
 router.delete("/:comment_id", function(req, res){
-  
+  Comment.findByIdAndRemove(req.params.comment_id, function(err){
+    if(err){
+      res.redirect("back");
+    } else {
+      res.redirect("/campgrounds/" + req.params.id);
+    }
+  });
 });
 
 function isLoggedIn(req, res, next){
