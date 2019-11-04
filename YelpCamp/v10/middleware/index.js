92d2bc0974e797b9b9da45ca59b8ffe03fd954dd
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-11-04 14:51:01 -0500

    copied isLoggedIn over

--- YelpCamp/v10/middleware/index.js
+++ YelpCamp/v10/middleware/index.js
@@ -36,4 +36,11 @@ middlewareObj.checkCommentOwnership = function checkCommentOwnership(req, res, n
   }
 }
 
+middlewareObj.isLoggedIn = function isLoggedIn(req, res, next){
+  if(req.isAuthenticated()){
+    return next();
+  }
+  res.redirect("/login");
+}
+
 module.exports = middlewareObj;
