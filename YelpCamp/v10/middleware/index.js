27a8cb1d41e07864d042e5c0c829e5a633c10306
Akhier Dragonheart <akherthedragonhearted@gmail.com>
2019-11-04 14:49:25 -0500

    copied over checkCommentOwnership

--- YelpCamp/v10/middleware/index.js
+++ YelpCamp/v10/middleware/index.js
@@ -1,12 +1,30 @@
 var middlewareObj = {};
 
-middlewareObj.checkCampgroundOwenership = function checkCampgroundOwnership(req, res, next){
+middlewareObj.checkCampgroundOwnership = function checkCampgroundOwnership(req, res, next){
   if(req.isAuthenticated()){
     Campground.findById(req.params.id, function(err, foundCampground){
       if(err){
         res.redirect("back");
       } else {
         if(foundCampground.author.id.equals(req.user._id)) {
+          next();
+        } else {
+          res.redirect("back");
+        }
+      }
+    });
+  } else {
+    res.redirect("back");
+  }
+}
+
+middlewareObj.checkCommentOwnership = function checkCommentOwnership(req, res, next){
+  if(req.isAuthenticated()){
+    Comment.findById(req.params.comment_id, function(err, foundComment){
+      if(err){
+        res.redirect("back");
+      } else {
+        if(foundComment.author.id.equals(req.user._id)) {
           next();
         } else {
           res.redirect("back");
